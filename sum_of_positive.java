16b6d168df36bd8e4d9b6cd298a6e600f27f5b85
Iurii Babarin <yuriyvaler@gmail.com>
2020-05-15 12:18:14 -0400

    Add solution

---
+++ sum_of_positive.java
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/5715eaedb436cf5606000381/java
+public class Positive {
+
+	  public static int sum(int[] arr){
+	    int sum = 0;
+	    for (int i = 0; i < arr.length; i++) {
+	    if (arr[i] > 0) sum += arr[i];
+	    }
+	    return sum;
+	  }
+	}
