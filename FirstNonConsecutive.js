714801ed267454f1e62588e8dbb196901513314e
Iurii Babarin <yuriyvaler@gmail.com>
2020-05-16 13:36:46 -0400

    Add solution

---
+++ FirstNonConsecutive.js
@@ -0,0 +1,17 @@
+//https://www.codewars.com/kata/58f8a3a27a5c28d92e000144/java
+class FirstNonConsecutive
+{
+    static Integer find(final int[] array)
+{
+    Integer result = null;
+    for (int i = 1; i < array.length; i++)
+{
+    if (array[i] - 1 != array[i - 1])
+{
+    result = array[i];
+    break;
+}
+}
+return result;
+}
+}
