d16cbf5140f7734a83f24d9fa7bf7b87f7cef3aa
Iurii Babarin <yuriyvaler@gmail.com>
2020-05-18 11:45:31 -0400

    Add solution

---
+++ GrassHopper.java
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/55d24f55d7dd296eb9000030
+public class GrassHopper
+{
+	public static int summation(int n)
+	{
+		int sum = 0;
+		for (int i = 1; i <= n; i++)
+		{
+			sum += i;
+		}
+		return sum;
+	}
+}
---
+++ YesOrNo.java
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/53369039d7ab3ac506000467
+class YesOrNo
+{
+  public static String boolToWord(boolean b)
+  {
+    return (b == true)? "Yes": "No";
+  }
+}
