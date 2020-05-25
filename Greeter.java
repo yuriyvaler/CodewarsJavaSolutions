bf49dc2c980f1ba60b613036b2acfd45c73dfb0f
Iurii Babarin <yuriyvaler@gmail.com>
2020-05-25 09:22:14 -0400

    Add solution

---
+++ Greeter.java
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/55225023e1be1ec8bc000390
+public class Greeter
+{
+	public static String greet(String name)
+	{
+		if (name.equals("Johnny"))
+			return "Hello, my love!";
+		else
+			return String.format("Hello, %s!", name);
+	}
+}
